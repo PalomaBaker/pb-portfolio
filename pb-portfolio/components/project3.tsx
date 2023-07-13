52ec476d0b53de3a2771af518a42a6c87a9ac4b6
Paloma Baker <paloma.d.baker@gmail.com>
2023-07-13 16:48:39 -0700

    adding project component templates

---
+++ pb-portfolio/components/project3.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface ProjectProps {
+  title: string;
+  description: string;
+  imageUrl?: string;
+  demoUrl?: string;
+  githubUrl?: string;
+}
+
+const Project3: React.FC<ProjectProps> = ({
+  title,
+  description,
+  imageUrl,
+  demoUrl,
+  githubUrl,
+}) => {
+  return (
+    <div className="project">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      {imageUrl && <img src={imageUrl} alt={title} />}
+      {demoUrl && (
+        <a href={demoUrl} target="_blank" rel="noopener noreferrer">
+          Demo
+        </a>
+      )}
+      {githubUrl && (
+        <a href={githubUrl} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      )}
+    </div>
+  );
+};
+
+export default Project3;
--- pb-portfolio/src/App.tsx
+++ pb-portfolio/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
+import Project1 from './components/Project1';
+import Project2 from './components/Project2';
+import Project3 from './components/Project3';
 
 const App = () => {
   return (
@@ -9,38 +12,15 @@ const App = () => {
       <main>
         <section id="projects">
           <h2>Projects</h2>
-          <Project // - update with project info 
-            title="Project 1"
-            description="Description of Project 1"
-            // Add any other relevant props for the project component
-          />
-          <Project
-            title="Project 2"
-            description="Description of Project 2"
-            // Add any other relevant props for the project component
-          />
-          <Project
-            title="Project 3"
-            description="Description of Project 3"
-            // Add any other relevant props for the project component
-          />
+          <Project1 />
+          <Project2 />
+          <Project3 />
         </section>
 
-        <section id="contact">
-          <h2>Contact</h2>
-          <form>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-            <button type="submit">Send</button>
-          </form>
-        </section>
+        {/* Rest of your code */}
       </main>
 
-      <footer>
-        <ul>
-          <li><a href="https://www.linkedin.com">LinkedIn</a></li>
-        </ul>
-      </footer>
+      {/* Footer code */}
     </div>
   );
 };
